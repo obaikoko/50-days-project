9210e6018591de2d5c4e9bb78a828c13c84d4b86
Obaikoko <jesseobinna7@gmail.com>
2022-04-30 08:47:52 +0100

    initial commit

---
+++ progress step/progress.js
@@ -0,0 +1,41 @@
+const circles = document.querySelectorAll('.circle')
+const progress = document.getElementById('progress')
+const next = document.getElementById('next')
+const prev = document.getElementById('prev')
+
+let currentActive = 1
+next.addEventListener('click', () =>{
+    currentActive++
+    if (currentActive > circles.length) {
+        currentActive = circles.length
+    }
+    update()
+})
+prev.addEventListener('click', () =>{
+    currentActive--
+    if (currentActive < 1) {
+        currentActive = 1
+    }
+    update()
+})
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active')
+        } else{
+            circle.classList.remove('active')
+        }
+        
+    })
+    progress.style.width = (((currentActive-1) / (circles.length-1)) * 100) + '%'
+    if (currentActive === circles.length) {
+        next.disabled = true
+    } else if (currentActive < circles.length){
+        next.disabled = false
+    }
+    if (currentActive > 1) {
+        prev.disabled = false
+    } else{
+        prev.disabled = true
+    }
+}
